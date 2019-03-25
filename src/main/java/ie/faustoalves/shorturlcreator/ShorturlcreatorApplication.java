acfa6e083cf7eb6ef923f3afd73eac76f8315701
FaustoTadeu <fausto.tna@hotmail.com>
2019-03-25 19:56:49 -0300

    First Commit

---
+++ src/main/java/ie/faustoalves/shorturlcreator/ShorturlcreatorApplication.java
@@ -0,0 +1,13 @@
+package ie.faustoalves.shorturlcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShorturlcreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShorturlcreatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/faustoalves/shorturlcreator/ShorturlcreatorApplicationTests.java
@@ -0,0 +1,16 @@
+package ie.faustoalves.shorturlcreator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShorturlcreatorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
